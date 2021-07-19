224515893ba0ec4eabc34d7a99f83cce6836cbf8
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-19 15:37:12 +0530

    Invoke methode using reflection

--- MoodAnalyser/MoodAnalyserFactory.cs
+++ MoodAnalyser/MoodAnalyserFactory.cs
@@ -59,5 +59,22 @@ namespace MoodAnalyserProblem
             }
             return default;
         }
+        public string InvokeAnalyserMethod(string message, string methodName)
+        {
+            try
+            {
+                Type type = typeof(MoodAnalyser);
+
+                MethodInfo methodInfo = type.GetMethod(methodName);
+                MoodAnalyserFactory factory = new MoodAnalyserFactory();
+                object moodAnalyserObject = factory.ParameterizedObject("MoodAnalyserProblem2.MoodAnalyser", "MoodAnalyser", message);
+                object info = methodInfo.Invoke(moodAnalyserObject, null);
+                return info.ToString();
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.METHOD_NOT_FOUND, "Method not found");
+            }
+        }
     }
 }
